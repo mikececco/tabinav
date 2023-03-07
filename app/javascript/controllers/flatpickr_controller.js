d60a9e82b564c16a1eb6930ca487f22c32e4e9fd
Mike Cecconello <mikececco2000@gmail.com>
2023-03-07 15:27:37 +0100

    Finished search bar with pretty date picker

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -1,15 +1,15 @@
-// app/javascript/controllers/flatpickr_controller.js
 import { Controller } from "@hotwired/stimulus"
 import flatpickr from "flatpickr";
+// Import the rangePlugin from the flatpickr library
+import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
 
 // Connects to data-controller="flatpickr"
 export default class extends Controller {
-  // Inform the controller that it has two targets in the form, which are our inputs
   static targets = [ "startDate", "endDate" ]
-
   connect() {
-    console.log("Hello connected");
-    flatpickr(this.startDateTarget, {})
+    flatpickr(this.startDateTarget, {
+              // Provide an id for the plugin to work
+              plugins: [new rangePlugin({ input: "#end_date"})]})
     flatpickr(this.endDateTarget, {})
   }
 }
