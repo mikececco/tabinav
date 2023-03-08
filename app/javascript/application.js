c13c99bb18fdc16f70f2acda5718db6f4ac4f05f
Mike Cecconello <mikececco2000@gmail.com>
2023-03-08 21:00:35 +0100

    Dynamic header based on scrolling

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -6,3 +6,14 @@ import "./controllers"
 // = require_tree .
 
 // *= require mapbox-gl-geocoder
+
+let prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  const currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.transform = "translateY(0)";
+  } else {
+    document.getElementById("navbar").style.transform = "translateY(-100%)";
+  }
+  prevScrollpos = currentScrollPos;
+}
