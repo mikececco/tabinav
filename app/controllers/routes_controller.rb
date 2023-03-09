976df1207ada61e9ef22ce38c57c6142a305dc56
Chi Yan Eirene CHAN <eirenechancy@gmail.com>
2023-03-09 18:13:54 +0100

    hotel

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -41,7 +41,8 @@ class RoutesController < ApplicationController
       I will be departing the #{route.start_date} and leaving on the #{route.end_date}.
       The itinerary will be #{no_of_days} days long. Distance with the previous day should be less than 100km.
       Give itinerary for each day.
-      Suggest places to visit within €#{route.budget}, convert all prices to euro.
+      Suggest places to visit and a hotel nearby within €#{route.budget}, convert all prices to euro.
+      Same hotel in the same city.
       Include coordinates of the places recommended.
       Specify the city and country.
       Use only english language.
@@ -49,31 +50,51 @@ class RoutesController < ApplicationController
       Be creative and inspire me.
       Respond with just the entire response in JSON.
       Example response for 2 stops:
-      [{
-        'day1': {
+      [
+        {'day1': {
           'city': 'Amsterdam',
           'country':'Netherlands',
-          'name': 'Van Gogh Museum',
-          'price': 20,
-          'description': 'The Van Gogh Museum is an art museum dedicated to the works of Vincent van Gogh and his contemporaries in Amsterdam in the Netherlands.',
-          'coordinates': {
-          'latitude': 52.358468,
-          'longitude': 4.881119
+          'activity': {
+            'name': 'Van Gogh Museum',
+            'price': 20,
+            'description': 'The Van Gogh Museum is an art museum dedicated to the works of Vincent van Gogh and his contemporaries in Amsterdam in the Netherlands.',
+            'coordinates': {
+            'latitude': 52.358468,
+            'longitude': 4.881119
+            }
           }
-        }
-      }, {
+          'hotel': {
+            'name': 'Crowne Plaza Amsterdam Zuid',
+            'price': 110,
+            'description': '5 star hotel with a sky bar and an indoor swimming pool.',
+            'coordinates': {
+            'latitude': 56.358468,
+            'longitude': 4.881119
+            }
+          }
+        }}, {
         'day2': {
           'city': 'Amsterdam',
-          'country':'Netherlands',
-          'name': 'Jordaan',
-          'price': 0,
-          'description': 'The Jordaan is a district in the citycenter of Amsterdam, known for its beautiful houses, nice restaurants and original shops. On the many bridges over the canals, you can take beautiful pictures and see why Amsterdam is called the Venice of the North.',
-          'coordinates': {
-          'latitude': 52.358468,
-          'longitude': 4.881119
+          'country': 'Netherlands',
+          'activity': {
+            'name': 'Jordaan',
+            'price': 0,
+            'description': 'Jordaan is a district in the citycenter of Amsterdam, known for its beautiful houses, nice restaurants and original shops. On the many bridges over the canals, you can take beautiful pictures and see why Amsterdam is called the Venice of the North.',
+            'coordinates': {
+            'latitude': 52.358468,
+            'longitude': 4.881119
+            }
           }
-        }
-      }]
+          'hotel': {
+            'name': 'Crowne Plaza Amsterdam Zuid',
+            'price': 110,
+            'description': '5 star hotel with a sky bar and an indoor swimming pool.',
+            'coordinates': {
+            'latitude': 56.358468,
+            'longitude': 4.881119
+            }
+          }
+        }}]
       Respond just with the JSON.
     ")
     @response = JSON.parse(@response)
@@ -81,17 +102,22 @@ class RoutesController < ApplicationController
     @response.each_with_index do |hash, i|
       day = Day.new(route: route)
 
-      day.name = hash["day#{i + 1}"]["name"]
-      day.description = hash["day#{i + 1}"]["description"]
-      day.price = hash["day#{i + 1}"]["price"]
-      day.latitude = hash["day#{i + 1}"]["coordinates"]["latitude"]
-      day.longitude = hash["day#{i + 1}"]["coordinates"]["longitude"]
+      day.name = hash["day#{i + 1}"]["activity"]["name"]
+      day.description = hash["day#{i + 1}"]["activity"]["description"]
+      day.price = hash["day#{i + 1}"]["activity"]["price"]
+      day.latitude = hash["day#{i + 1}"]["activity"]["coordinates"]["latitude"]
+      day.longitude = hash["day#{i + 1}"]["activity"]["coordinates"]["longitude"]
       day.city = hash["day#{i + 1}"]["city"]
-      day.country = hash["day#{i + 1}"]["country"]
+      day.nation = hash["day#{i + 1}"]["country"]
+      day.name_hotel = hash["day#{i + 1}"]["hotel"]["name"]
+      day.description_hotel = hash["day#{i + 1}"]["hotel"]["description"]
+      day.price_hotel = hash["day#{i + 1}"]["hotel"]["price"]
+      day.latitude_hotel = hash["day#{i + 1}"]["hotel"]["coordinates"]["latitude"]
+      day.longitude_hotel = hash["day#{i + 1}"]["hotel"]["coordinates"]["longitude"]
       day.save
 
-      country_array << day.country unless country_array.include?(day.country)
-      @route.total_price += day.price
+      country_array << day.nation unless country_array.include?(day.nation)
+      @route.total_price += ( day.price + day.price_hotel)
     end
     assign_destination_to_route(route, country_array)
   end
