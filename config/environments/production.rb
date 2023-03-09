b553fa03772b4e00d653121e49e3a31ee62a7525
Jeroen Hesp <jeroen.hesp@gmail.com>
2023-03-09 15:45:30 +0100

    Last Email Changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,7 +8,7 @@ Rails.application.configure do
 
   # To get email service working in production
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: "www.aqueous-depths-12365.herokuapp.com/" }
+  config.action_mailer.default_url_options = { host: "www.aqueous-depths-12365.herokuapp.com" }
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
