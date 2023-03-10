69972e47ea038842087cf77e5a6948b3c480a2b6
Jeroen Hesp <jeroen.hesp@gmail.com>
2023-03-10 15:31:50 +0100

    Animation added in previous commit, but not working in my local, need to test on heroku

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,29 +1,13 @@
-// Entry point for the build script in your package.json
-
-//=require_tree
+/ Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "flowbite"
 import "flowbite/dist/flowbite.turbo.js";
 import mapboxgl from 'mapbox-gl';
-import { displayLogo } from "./searchanimation"
-
-console.log("asfasf")
-
-document.addEventListener("turbo:load", () => {
-  const btn = document.querySelector(".form-submit")
-  console.log(btn)
-  btn.addEventListener("click", () => {
-    displayLogo()
-  })
-})
-
 //
 // *= require mapbox-gl
 // = require_tree .
-
 // *= require mapbox-gl-geocoder
-
 // let prevScrollpos = window.pageYOffset;
 // window.onscroll = function() {
 //   const currentScrollPos = window.pageYOffset;
@@ -34,21 +18,29 @@ document.addEventListener("turbo:load", () => {
 //   }
 //   prevScrollpos = currentScrollPos;
 // }
-
 $(document).ready(function() {
   // When hovering over the main content div, hide the navbar
   $('#main-content').hover(function() {
     $('#navbar').addClass('bg-darkerYellow');
     $('.navbar-element').addClass('opacity-0');
     $("#powered_by").removeClass('opacity-0');
-
     // $('#budgetInput').addClass('placeholder-black')
   }, function() {
     $('.navbar-element').removeClass('opacity-0');
     $('#navbar').removeClass('bg-darkerYellow');
     // $('#budgetInput').removeClass('placeholder-black')
     $("#powered_by").addClass('opacity-0');
-
-
   });
 });
+
+
+
+
+
+
+
+
+
+
+
+
