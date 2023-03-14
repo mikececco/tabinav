852d88b76a8f9d9aac99e870122048f642363616
Jeroen Hesp <jeroen.hesp@gmail.com>
2023-03-14 11:59:10 +0100

    Some more changes to BookingConf

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,7 +1,6 @@
 class BookingsController < ApplicationController
   def index
     @bookings = current_user.bookings.order(created_at: :desc)
-
   end
 
   def show
@@ -17,18 +16,11 @@ class BookingsController < ApplicationController
     @bookmark = Bookmark.find(params[:bookmark_id])
     @booking = Booking.new(bookmark: @bookmark)
     if @booking.save
-<<<<<<< HEAD
-    #   mail = User.Mailer.with(user: current_user).welcome.deliver_now
-      redirect_to booking_path(@booking)
-    # else
-    #   raise
-=======
       # mail = User.Mailer.with(user: current_user).welcome.deliver_now
       pack_advice(@booking)
       redirect_to booking_path(@booking)
     else
       raise
->>>>>>> master
     end
   end
 
@@ -38,11 +30,11 @@ class BookingsController < ApplicationController
     redirect_back(fallback_location: root_path)#, status: :see_other
   end
 
-<<<<<<< HEAD
   def show
     @booking = Booking.find(params[:id])
     @route = @booking.bookmark.route
-=======
+  end
+
   private
 
   def pack_advice(booking)
@@ -53,6 +45,5 @@ class BookingsController < ApplicationController
       - Lightweight and comfortable clothes: The weather in the Netherlands in May is usually mild, with average temperatures ranging from 10°C to 17°C. Pack clothes that can be layered to accommodate changes in temperature.\n\n
       - Rain jacket or umbrella: May is one of the rainiest months in the Netherlands, so it's a good idea to pack a waterproof jacket or umbrella.")
     @booking.save
->>>>>>> master
   end
 end
