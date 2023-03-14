82a18b31332adc204d1eb090af2b8850a71868cf
Mike Cecconello <mikececco2000@gmail.com>
2023-03-14 14:31:21 +0100

    Added labels

--- app/javascript/controllers/gpt_response_controller.js
+++ app/javascript/controllers/gpt_response_controller.js
@@ -2,94 +2,66 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="gpt-response"
 export default class extends Controller {
+  static targets = ["nations", "cities"]
+
   connect() {
     console.log("HELlo");
 
     const text = `
-    I want to go on a trip around Amsterdam.
-    I will be departing the 10th of March and leaving on the 15th of March.
-    The itinerary will be #{no_of_days} days long.
-    Give itinerary for each day. Take travelling time into account.
-    Suggest places to visit, convert all prices to euro.
-    Choose #{@route.hotel_pref}.
-    Same hotel in the same city.
-    Include coordinates of the places recommended.
-    Specify the city and country.
-    Use only english language.
-    Keep the description of the place within 250-300 characters.
-    Be creative and inspire me.
-    Respond with just the entire response in JSON.
-    Example response for 2 stops:
+    I want to go on a vacation in Paris.
+    Suggest 4 cities around to visit.
+    Specify the country.
+    Respond with just the entire response in JSON with a total of 4 days.
+    Example response for 3 cities with a total of 8 days:
     [
-      {'day1': {
-        'city': 'Amsterdam',
-        'country':'Netherlands',
-        'activity': {
-          'name': 'Van Gogh Museum',
-          'price': 20,
-          'description': 'The Van Gogh Museum is an art museum dedicated to the works of Vincent van Gogh and his contemporaries in Amsterdam in the Netherlands.',
-          'coordinates': {
-          'latitude': 52.358468,
-          'longitude': 4.881119
-          }
-        }
-        'hotel': {
-          'name': 'Crowne Plaza Amsterdam Zuid',
-          'room_type': 'Twin Room',
-          'no_of_people_per_room': 2,
-          'price': 110,
-          'description': '5 star hotel located in the east of Amsterdam, it has a lively bar, a private garden, a restaurant and a terrace. It is within walking distance of many restaurants, bars and clubs.',
-          'coordinates': {
-          'latitude': 56.358468,
-          'longitude': 4.881119
-          }
-        }
-      }}, {
-      'day2': {
-        'city': 'Amsterdam',
-        'country': 'Netherlands',
-        'activity': {
-          'name': 'Jordaan',
-          'price': 0,
-          'description': 'Jordaan is a district in the citycenter of Amsterdam, known for its beautiful houses, nice restaurants and original shops. On the many bridges over the canals, you can take beautiful pictures and see why Amsterdam is called the Venice of the North.',
-          'coordinates': {
-          'latitude': 52.358468,
-          'longitude': 4.881119
-          }
-        }
-        'hotel': {
-          'name': 'Crowne Plaza Amsterdam Zuid',
-          'room_type': 'Twin Room',
-          'no_of_people_per_room': 2,
-          'price': 110,
-          'description': '5 star hotel with a sky bar and an indoor swimming pool.',
-          'coordinates': {
-          'latitude': 56.358468,
-          'longitude': 4.881119
-          }
-        }
-      }}]
-    Respond just with the JSON.
-  `
+      { 'city'=>'Amsterdam', 'country'=> 'Netherlands', 'days'=> 3 },
+      { 'city'=>'Rotterdam', 'country'=> 'Netherlands', 'days'=> 3 },
+      { 'city'=>'Brussels', 'country'=> 'Belgium', 'days'=> 2 }
+    ]
+    Respond just with the JSON.`
 
-    console.log("HELlo");
     const url = 'https://api.openai.com/v1/chat/completions'
     fetch(url, {
       method: "POST",
       headers: {
         'Content-Type': 'application/json',
-        'Authorization': "Bearer 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"
+        'Authorization': "Bearer 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"
       },
       body: JSON.stringify({
         "model": 'gpt-3.5-turbo',
         "messages": [{ role: 'user', content: text }]
       })
-    }).then(response => response.text())
-      .then((data) => {
-        console.log(data);
-      })
-
-    console.log("HELlo");
-
+    }).then(response => response.json())
+    .then(data => this.addNationsToLabel(data))
+    .then(data => this.addCitiesToList(data))
   }
+
+      addNationsToLabel(data){
+        let resp = data['choices'][0]['message']['content'];
+        resp = JSON.parse(resp);
+        // nationsTargets.insertAdjacentHTML('afterbegin', )
+        resp.forEach((obj) => {
+          let nationDiv = `<div class="inline-block px-2 py-1 mr-2 mb-2 px-3 py-1 leading-none bg-orange-200 text-orange-800 rounded-full font-semibold uppercase tracking-wide text-xs">${obj['country']}</div>`;
+          this.nationsTarget.insertAdjacentHTML('afterbegin', nationDiv);
+        })
+        console.log("Finished with nations");
+      }
+      // .then(data => console.log(data['choices'][0]['message']['content']));
+      // .then(data => this.nationsTarget.innerText = data['choices'][0]['message']['content'][0]['city'])
+      addCitiesToList(data){
+        if (!data || !data.choices) {
+          console.error('Invalid data format');
+          return;
+        }
+        let resp = data['choices'][0]['message']['content'];
+        resp = JSON.parse(resp);
+        console.log(resp);
+        // nationsTargets.insertAdjacentHTML('afterbegin', )
+        resp.forEach((element, index) => {
+          console.log(element);
+          let citiesList = `<li class="text-lg font-semibold">Day ${index + 1} - ${element['city']}, ${element['country']}</li>`
+          this.citiesTarget.insertAdjacentHTML('afterbegin', citiesList);
+        // this.citiesTarget.adjacentHTML("beforeend",data['choices'][0]['message']['content'][0]['city']
+      })
+    }
 }
