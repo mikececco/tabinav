bf274ff2f44ece40c3672d15f802187e9f343f91
Chi Yan Eirene CHAN <eirenechancy@gmail.com>
2023-03-17 09:20:31 +0100

    hardcoding hotel

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -125,43 +125,71 @@ class DaysController < ApplicationController
   def downgrade_hotel
     @day = Day.find(params[:day_id])
     old_price = @day.price_hotel
-    @response = ChatgptService.call("
-      I want to find another hotel in #{@day.city} with price about #{@day.price_hotel * 0.8}.
-      Convert all prices to euro.
-      Include coordinates of the places recommended.
-      Use only english language.
-      Keep the description of the place within 200-300 characters.
-      Be creative and inspire me.
-      Respond with just the entire response in JSON.
-      Example response:
-      [{
-        'name': 'Crowne Plaza Amsterdam Zuid',
-        'room_type': 'Twin Room',
+    if @day.city == "Queenstown"
+      hash = {
+        'name': 'The Rees Hotel & Luxury Apartments',
+        'room_type': 'Deluxe Lakeview Hotel Room',
         'no_of_people_per_room': 2,
-        'price': 110,
-        'description': '5 star hotel located in the east of Amsterdam, it has a lively bar, a private garden, a restaurant and a terrace. It is within walking distance of many restaurants, bars and clubs.',
+        'price': 257.9,
+        'description': 'Experience luxury and stunning views of Lake Wakatipu at The Rees Hotel. Enjoy the exceptional service, fine dining, and spacious rooms and suites. Take a short walk to the Queenstown Gardens or hop on a shuttle to reach the town center easily.',
         'coordinates': {
-        'latitude': 56.358468,
-        'longitude': 4.881119
+        'latitude': -45.03228456452642,
+        'longitude': 168.66300402905625
         }
-      }]
-      Respond just with the JSON.
-    ")
-    hash = JSON.parse(@response).first
-
-    route = @day.route
-    @days = Day.where(route: route, name_hotel: @day.name_hotel)
-    @days.each do |day|
-      day.name_hotel = hash["name"]
-      day.description_hotel = hash["description"]
-      day.price_hotel = hash["price"]
-      day.room_type = hash["room_type"]
-      day.no_of_rooms = day.route.no_of_people.fdiv(hash["no_of_people_per_room"]).ceil
-      day.latitude_hotel = hash["coordinates"]["latitude"]
-      day.longitude_hotel = hash["coordinates"]["longitude"]
-      if day.save
-        route.total_price += (day.price_hotel - old_price) * day.no_of_rooms
+      }
+      route = @day.route
+      @days = Day.where(route: route, name_hotel: @day.name_hotel)
+      @days.each do |day|
+        day.name_hotel = hash[:name]
+        day.description_hotel = hash[:description]
+        day.price_hotel = hash[:price]
+        day.room_type = hash[:room_type]
+        day.no_of_rooms = day.route.no_of_people.fdiv(hash[:no_of_people_per_room]).ceil
+        day.latitude_hotel = hash[:coordinates][:latitude]
+        day.longitude_hotel = hash[:coordinates][:longitude]
+        if day.save
+          route.total_price += (day.price_hotel - old_price) * day.no_of_rooms
+        end
       end
+    else
+      @response = ChatgptService.call("
+        I want to find another hotel in #{@day.city} with price about #{@day.price_hotel * 0.8}.
+        Convert all prices to euro.
+        Include coordinates of the places recommended.
+        Use only english language.
+        Keep the description of the place within 200-300 characters.
+        Be creative and inspire me.
+        Respond with just the entire response in JSON.
+        Example response:
+        [{
+          'name': 'Crowne Plaza Amsterdam Zuid',
+          'room_type': 'Twin Room',
+          'no_of_people_per_room': 2,
+          'price': 110,
+          'description': '5 star hotel located in the east of Amsterdam, it has a lively bar, a private garden, a restaurant and a terrace. It is within walking distance of many restaurants, bars and clubs.',
+          'coordinates': {
+          'latitude': 56.358468,
+          'longitude': 4.881119
+          }
+        }]
+        Respond just with the JSON.
+      ")
+      hash = JSON.parse(@response).first
+      route = @day.route
+      @days = Day.where(route: route, name_hotel: @day.name_hotel)
+      @days.each do |day|
+        day.name_hotel = hash["name"]
+        day.description_hotel = hash["description"]
+        day.price_hotel = hash["price"]
+        day.room_type = hash["room_type"]
+        day.no_of_rooms = day.route.no_of_people.fdiv(hash["no_of_people_per_room"]).ceil
+        day.latitude_hotel = hash["coordinates"]["latitude"]
+        day.longitude_hotel = hash["coordinates"]["longitude"]
+        if day.save
+          route.total_price += (day.price_hotel - old_price) * day.no_of_rooms
+        end
+      end
+
     end
     route.save
     respond_to do |format|
@@ -178,39 +206,64 @@ class DaysController < ApplicationController
     current_activities = []
     @days.each { |day| current_activities << day.name }
 
-    @response = ChatgptService.call("
-      I want to find another place to visit in #{@day.city} except #{current_activities.join(", ")}.
-      Convert all prices to euro. Price can be 0.
-      Include coordinates of the places recommended.
-      Use only english language.
-      Keep the description of the place within 200-300 characters.
-      Be creative and inspire me.
-      Respond with just the entire response in JSON.
-      Example response:
-      [{
-        'name': 'Grand Place',
-        'price': 20,
-        'description': 'Much of the square's elegant character is due to the unique architecture of its elegant Gildehuizen (guild houses) with their magnificent gables, pilasters, and balustrades, ornately carved stonework, and rich gold decoration.',
+    if @day.city == "Auckland"
+      hash = {
+        'name': 'Waiheke Island',
+        'price': 40,
+        'description': 'Take a ferry to Waiheke Island and enjoy the beautiful beaches and wineries. This island is known for its laid-back vibe and stunning scenery.',
         'coordinates': {
-          'latitude': 52.358468,
-          'longitude': 4.881119
+          'latitude': -36.791,
+          'longitude': 175.0989
         }
-      }]
-      Respond just with the JSON.
-    ")
-    hash = JSON.parse(@response).first
+      }
 
-    @day.name = hash["name"]
-    @day.description = hash["description"]
-    @day.price = hash["price"]
-    @day.latitude = hash["coordinates"]["latitude"]
-    @day.longitude = hash["coordinates"]["longitude"]
-    if @day.save
-      route.total_price += (@day.price - old_price) * route.no_of_people
+      @day.name = hash[:name]
+      @day.description = hash[:description]
+      @day.price = hash[:price]
+      @day.latitude = hash[:coordinates][:latitude]
+      @day.longitude = hash[:coordinates][:longitude]
+      if @day.save
+        route.total_price += (@day.price - old_price) * route.no_of_people
+      end
+
+      route.save
+      redirect_to route_path(route), status: :see_other
+    else
+      @response = ChatgptService.call("
+        I want to find another place to visit in #{@day.city} except #{current_activities.join(", ")}.
+        Convert all prices to euro. Price can be 0.
+        Include coordinates of the places recommended.
+        Use only english language.
+        Keep the description of the place within 200-300 characters.
+        Be creative and inspire me.
+        Respond with just the entire response in JSON.
+        Example response:
+        [{
+          'name': 'Grand Place',
+          'price': 20,
+          'description': 'Much of the square's elegant character is due to the unique architecture of its elegant Gildehuizen (guild houses) with their magnificent gables, pilasters, and balustrades, ornately carved stonework, and rich gold decoration.',
+          'coordinates': {
+            'latitude': 52.358468,
+            'longitude': 4.881119
+          }
+        }]
+        Respond just with the JSON.
+      ")
+      hash = JSON.parse(@response).first
+
+      @day.name = hash["name"]
+      @day.description = hash["description"]
+      @day.price = hash["price"]
+      @day.latitude = hash["coordinates"]["latitude"]
+      @day.longitude = hash["coordinates"]["longitude"]
+      if @day.save
+        route.total_price += (@day.price - old_price) * route.no_of_people
+      end
+
+      route.save
+      redirect_to route_path(route), status: :see_other
     end
 
-    route.save
-    redirect_to route_path(route), status: :see_other
   end
 
   def update_sequence
